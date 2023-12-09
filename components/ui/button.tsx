8ee112198960654bfc38c7522e4d465ad1513be6
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-12-09 22:56:38 +0530

    adding list and board completed

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        transparent: "bg-transparent text-white hover:bg-white/20",
         primary: "bg-[#DA0037]  hover:bg-[#DA0037]/90 text-primary-foreground ",
         custom: "bg-black shadow-sm shadow-dark_red ",
       },
