518cf0da5a2e521065e86991265e1bc68069e3fb
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-12-10 02:48:53 +0530

    adding things

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
@@ -36,11 +36,11 @@ export const ActivityList = async () => {
 ActivityList.Skeleton = function ActivityListSkeleton() {
   return (
     <ol className="space-y-4 mt-4">
-      <Skeleton className="w-[80%] h-14" />
-      <Skeleton className="w-[50%] h-14" />
-      <Skeleton className="w-[70%] h-14" />
-      <Skeleton className="w-[80%] h-14" />
-      <Skeleton className="w-[75%] h-14" />
+      <Skeleton className="w-[80%] bg-brown  h-14" />
+      <Skeleton className="w-[50%]  bg-brown  h-14" />
+      <Skeleton className="w-[70%] bg-brown  h-14" />
+      <Skeleton className="w-[80%] bg-brown  h-14" />
+      <Skeleton className="w-[75%] bg-brown  h-14" />
     </ol>
   );
 };
